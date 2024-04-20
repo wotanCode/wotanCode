1ed2825f1c7a97c1504527eec32fb687d16eae83
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2024-04-19 23:43:33 -0400

    prettier

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
-import { promises as fs } from 'fs'
-import fetch from 'node-fetch'
+import { promises as fs } from 'fs';
+import fetch from 'node-fetch';
 
-import { YOUTUBE_WOTANCODE_CHANNEL_ID, NUMBER_OF, PLACEHOLDERS } from './constants.js'
+import {
+  YOUTUBE_WOTANCODE_CHANNEL_ID,
+  NUMBER_OF,
+  PLACEHOLDERS,
+} from './constants.js';
 
-const {
-  YOUTUBE_API_KEY
-} = process.env
+const { YOUTUBE_API_KEY } = process.env;
 
-const getLatestYoutubeVideos = ({ channelId } = { channelId: YOUTUBE_WOTANCODE_CHANNEL_ID }) =>
+const getLatestYoutubeVideos = (
+  { channelId } = { channelId: YOUTUBE_WOTANCODE_CHANNEL_ID }
+) =>
   fetch(
     `https://www.googleapis.com/youtube/v3/search?key=${YOUTUBE_API_KEY}&channelId=${channelId}&part=snippet,id&order=date&maxResults=${NUMBER_OF.VIDEOS}`
   )
     .then((res) => res.json())
-    .then((videos) => videos.items)
+    .then((videos) => videos.items);
 
 const generateYoutubeHTML = ({ title, videoId }) => `
 <a href='https://youtu.be/${videoId}' target='_blank'>
@@ -22,19 +26,22 @@ const generateYoutubeHTML = ({ title, videoId }) => `
 (async () => {
   const [template, youtubeVideosResponse] = await Promise.all([
     fs.readFile('./src/README.md.tpl', { encoding: 'utf-8' }),
-    getLatestYoutubeVideos()
-  ])
+    getLatestYoutubeVideos(),
+  ]);
 
   const latestYoutubeVideos = youtubeVideosResponse
     .map(({ id, snippet }) => {
-      const { title } = snippet
-      const { videoId } = id
+      const { title } = snippet;
+      const { videoId } = id;
 
-      return generateYoutubeHTML({ videoId, title })
+      return generateYoutubeHTML({ videoId, title });
     })
-    .join('')
+    .join('');
 
-  const newMarkdown = template.replace(PLACEHOLDERS.LATEST_YOUTUBE, latestYoutubeVideos)
+  const newMarkdown = template.replace(
+    PLACEHOLDERS.LATEST_YOUTUBE,
+    latestYoutubeVideos
+  );
 
-  await fs.writeFile('README.md', newMarkdown)
-})()
+  await fs.writeFile('README.md', newMarkdown);
+})();
