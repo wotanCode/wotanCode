9d8e1e68c6d8b42dc69c920872815d9713c1b99f
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2024-04-19 23:43:20 -0400

    update number of videos

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,9 @@
-export const YOUTUBE_WOTANCODE_CHANNEL_ID = 'UCwISu2hFg7EpOIZ8aV7iS6g'
+export const YOUTUBE_WOTANCODE_CHANNEL_ID = 'UCwISu2hFg7EpOIZ8aV7iS6g';
 
 export const NUMBER_OF = {
-  VIDEOS: 3
-}
+  YOUTUBE_VIDEOS: 6,
+};
 
 export const PLACEHOLDERS = {
-  LATEST_YOUTUBE: '%{{latest_youtube}}%'
-}
+  LATEST_YOUTUBE: '%{{latest_youtube}}%',
+};
