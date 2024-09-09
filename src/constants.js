9d8f58b71023cc40e15591ae84cb0fd10c6f0816
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2024-09-09 16:13:43 -0300

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,12 @@
 export const YOUTUBE_PEDROELHUMANO_CHANNEL_ID = 'UCwISu2hFg7EpOIZ8aV7iS6g'
 
+export const GITHUB_PEDROELHUMANO_USERNAME = 'pedroelhumano'
+
 export const NUMBER_OF = {
   YOUTUBE_VIDEOS: 6
 }
 
 export const PLACEHOLDERS = {
-  LATEST_YOUTUBE: '%{{latest_youtube}}%'
+  LATEST_YOUTUBE: '%{{latest_youtube}}%',
+  GITHUB_PROFILE_AVATAR: '%{{github_profile_avatar}}%'
 }
